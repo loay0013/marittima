2c0fa1b9c8cb57a15829366a925e676df7e40e09
loayhajjo <louy0013@edu.zealand.dk>
2022-05-20 11:49:25 +0200

    forside1.0

--- Forside.php
+++ Forside.php
@@ -48,12 +48,141 @@
     </a>
 </div>
 
-
-
-
 </header>
 
 
+<section>
+    <div>
+        <img src="images/logosortudenslogan%20.svg">
+        <p>
+            Hos Marittima ønsker vi at skabe en god oplevelse for hele familien.
+
+            Marittima blev skabt ud fra drømmen om en vinbar. Historien bag Marius Mortensen går hånd i hånd med Skibet Marittima, og fører den næste generations historie videre.
+
+            Folk der har været på skibet vil være i stand til at kunne fortælle om historier om dengang de holdt fest på bølgen blå, med den synlige solnedgang ud af vinduet, lige før den når halvvejs under horisonten.
+
+        </p>
+    </div>
+    <div>
+        <a class="btn btn-minFarve">
+          <p>
+              LÆS MERE
+          </p>
+        </a>
+    </div>
+    <img src="images/#" alt="vin">
+
+</section>
+
+         <section>
+    <h2>Private Arrangementer</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+
+        At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+
+        At vero eos et accusam et justo duo dolores et ea
+    </p>
+    <a class=" btn ">
+        <p class="">
+            BOOK NU
+        </p>
+    </a>
+<img src="images/#" alt="vin">
+
+         </section>
+<section>
+    <h2>
+        VINOTEQUE
+    </h2>
+    <p>
+        Strandvejen 28, 4800 Nykøbing Falster
+    </p>
+    <h2>
+        Åbningstider
+    </h2>
+<div>
+    <div>
+    <ul>
+        <li>
+            <p>
+                Mandag
+            </p>
+        </li>
+        <li>
+            <p>
+                Tirsdag
+            </p>
+        </li>
+        <li>
+            <p>
+                Onsdag
+            </p>
+        </li>
+        <li>
+            <p>
+                Torsdag
+            </p>
+        </li>
+        <li>
+            <p>
+                Fredag
+            </p>
+        </li>
+        <li>
+            <p>
+                Lørdag
+            </p>
+        </li>
+        <li>
+            <p>
+                Søndag
+            </p>
+        </li>
+
+
+    </ul>
+    </div>
+    <div>
+        <ul>
+            <li>
+                <p>
+                    18.00 - 02.30
+                </p>
+            </li>
+            <li>
+                <p>
+                    18.00 - 02.30
+                </p>
+            </li>
+            <li>
+                <p>
+                    18.00 - 02.30
+                </p>
+            </li>
+            <li>
+                <p>
+                    18.00 - 02.30
+                </p>
+            </li>
+            <li>
+                <p>
+                    18.00 - 02.30
+                </p>
+            </li>
+            <li>
+                <p>
+                    18.00 - 02.30
+                </p>
+            </li>
+            <li>
+                <p>
+                    18.00 - 02.30
+                </p>
+            </li>
+    </div>
+</div>
+</section>
 
 
 
@@ -64,4 +193,5 @@
 
 </body>
 </html>
+
 
